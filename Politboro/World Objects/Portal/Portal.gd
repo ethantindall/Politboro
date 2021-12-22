528c12fd5cf7c889555c44c2f0622051bd2824d6
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-12-22 15:40:20 -0700

    first

---
+++ Politboro/World Objects/Portal/Portal.gd
@@ -0,0 +1,22 @@
+tool
+
+extends Area2D
+
+export(String, FILE) var next_scene_path = ""
+#export(Vector2) var player_spawn_location = Vector2.ZERO
+
+func _get_configuration_warning() -> String:
+	if next_scene_path == "":
+		return "next_scene path not set"
+	else:
+		return ""
+
+
+func _on_Portal_body_entered(_body):
+	ScreenChanger.change_scene(next_scene_path)
+
+	print($"/root/Global".player.get_position())
+	$"/root/Global".player.set_position(Vector2(100,100))
+	
+
+
