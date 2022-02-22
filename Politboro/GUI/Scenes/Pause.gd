e9b63a484edebedf45679814669e5f5eb5d3f002
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2022-02-22 14:12:32 -0700

    Before menu change

--- Politboro/GUI/Scenes/Pause.gd
+++ Politboro/GUI/Scenes/Pause.gd
@@ -5,13 +5,31 @@ var generalSprite = preload("res://Images/Sprites/General.png")
 var valerySprite = preload("res://Images/Sprites/Judge.png")
 var petyaSprite = preload("res://Images/Sprites/OfficialB.png")
 var glebSprite = preload("res://Images/Sprites/OfficialC.png")
+var result_json
 
 func _input(event):
 	if event.is_action_pressed("pause"):
 		var new_pause_state = not get_tree().paused
 		get_tree().paused = new_pause_state
 		visible = new_pause_state
-	
+		
+		#GET JSON DATA
+		var file = File.new()
+		file.open("res://GUI/npcs.json", file.READ)
+		var text_json = file.get_as_text()
+		result_json = JSON.parse(text_json)
+		#print(processJSON(result_json).NPCs.Ministers.A.Name)
+
+		
+
+func processJSON(rj):
+	if rj.error == OK:  # If parse OK
+		var data = result_json.result
+		return data
+	else:  # If parse has errors
+		print("Error: ", rj.error)
+		print("Error Line: ", rj.error_line)
+		print("Error String: ", rj.error_string)
 
 #Level1
 func _on_PeopleButton_pressed():
@@ -34,31 +52,32 @@ func _on_ItemsButton_pressed():
 	
 #Level2
 func _on_VladButton_pressed():
-	get_node("Folder/BioControl/NPCname").text = "Vlad Vladostovovitch"
-	get_node("Folder/BioControl/NPCbio").text = "Vlad came to power after his predecessor died in a mysterious nuclear accident."
+	get_node("Folder/BioControl/NPCname").text = str(processJSON(result_json).NPCs.Ministers.A.Name)
+	get_node("Folder/BioControl/NPCbio").text = str(processJSON(result_json).NPCs.Ministers.A.Bio)
 	get_node("Folder/BioControl/Photo/ColorRect/NPCimage").set_texture(vladSprite)
-	get_node("Folder/BioControl/NPCtitle").text = "Minister of Energy"
+	get_node("Folder/BioControl/NPCtitle").text = str(processJSON(result_json).NPCs.Ministers.A.Title)
+
 	
 func _on_GeneralButton_pressed():
-	get_node("Folder/BioControl/NPCname").text = "General Aleksi Butunov"
-	get_node("Folder/BioControl/NPCbio").text = "A loyal nationalist. Highly admired by his troops. They will follow him to the grave."
+	get_node("Folder/BioControl/NPCname").text = str(processJSON(result_json).NPCs.Ministers.B.Name)
+	get_node("Folder/BioControl/NPCbio").text = str(processJSON(result_json).NPCs.Ministers.B.Bio)
 	get_node("Folder/BioControl/Photo/ColorRect/NPCimage").set_texture(generalSprite)
-	get_node("Folder/BioControl/NPCtitle").text = "Minister of War"
+	get_node("Folder/BioControl/NPCtitle").text = str(processJSON(result_json).NPCs.Ministers.B.Title)
 		
 func _on_ValeryButton_pressed():
-	get_node("Folder/BioControl/NPCname").text = "Valery Unchok"
-	get_node("Folder/BioControl/NPCbio").text = "Minister of Justice is a misnomer if ever their was one. His sole responsibility is to run the Secret Police."
+	get_node("Folder/BioControl/NPCname").text = str(processJSON(result_json).NPCs.Ministers.C.Name)
+	get_node("Folder/BioControl/NPCbio").text = str(processJSON(result_json).NPCs.Ministers.C.Bio)
 	get_node("Folder/BioControl/Photo/ColorRect/NPCimage").set_texture(valerySprite)
-	get_node("Folder/BioControl/NPCtitle").text = "Minister of Justice"
+	get_node("Folder/BioControl/NPCtitle").text = str(processJSON(result_json).NPCs.Ministers.C.Title)
 	
 func _on_PetyaButton_pressed():
-	get_node("Folder/BioControl/NPCname").text = "Petya Belov"
-	get_node("Folder/BioControl/NPCbio").text = "From a long family of bankers. Rumor has it he has ties to the international banking community."
+	get_node("Folder/BioControl/NPCname").text = str(processJSON(result_json).NPCs.Ministers.D.Name)
+	get_node("Folder/BioControl/NPCbio").text = str(processJSON(result_json).NPCs.Ministers.D.Bio)
 	get_node("Folder/BioControl/Photo/ColorRect/NPCimage").set_texture(petyaSprite)
-	get_node("Folder/BioControl/NPCtitle").text = "Minister of Finance"
+	get_node("Folder/BioControl/NPCtitle").text = str(processJSON(result_json).NPCs.Ministers.D.Title)
 	
 func _on_GlebButton_pressed():
-	get_node("Folder/BioControl/NPCname").text = "Gleb Tarasov"
-	get_node("Folder/BioControl/NPCbio").text = "He makes the trains run on time, but is primarily famous for his use of slave labor to build infrastructure."
+	get_node("Folder/BioControl/NPCname").text = str(processJSON(result_json).NPCs.Ministers.E.Name)
+	get_node("Folder/BioControl/NPCbio").text = str(processJSON(result_json).NPCs.Ministers.E.Bio)
 	get_node("Folder/BioControl/Photo/ColorRect/NPCimage").set_texture(glebSprite)
-	get_node("Folder/BioControl/NPCtitle").text = "Minister of Transportation"
+	get_node("Folder/BioControl/NPCtitle").text = str(processJSON(result_json).NPCs.Ministers.E.Title)
