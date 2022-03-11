1b1c5de930221d730e71a7f2df6d713b7db1d037
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2022-03-11 12:28:14 -0700

    items

--- Politboro/GUI/Scenes/Pause/InventoryMenu.gd
+++ Politboro/GUI/Scenes/Pause/InventoryMenu.gd
@@ -8,9 +8,27 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_ScrollContainer_visibility_changed():
+	if visible == true:
+		var itemNumber = 0
+		var grid = get_node("ColorRect4/ScrollContainer/GridContainer")
+		var arraySize = Global.player_inventory.size()
+		for slot in grid.get_children():
+			if itemNumber < arraySize:
+				slot.get_node("Sprite").set_texture(load(Global.player_inventory[itemNumber]))
+			itemNumber +=1
+			
+		
+
+
+
+
+func _on_Slot1_mouse_entered():
+	pass # Replace with function body.
--- Politboro/World Objects/Accessories/InvItems/invitem.gd
+++ Politboro/World Objects/Accessories/InvItems/invitem.gd
@@ -1,16 +1,39 @@
 extends StaticBody2D
 
 export(String) var itemName = ""
+export(String, FILE) var imagePath = ""
+export(String) var description = ""
+export(String) var pickupTimeline = ""
 export var value = 0
 var active = false
-
+"""
 func _input(event):
 		if event.is_action_pressed("ui_accept") and active:
 			print("picked up")
 			get_parent().remove_child(self)
-			Global.player_inventory.append(itemName)
+			Global.player_inventory.append(imagePath)
 			print(Global.player_inventory)
+"""		
 			
+func _input(event):
+	if get_node_or_null('DialogNode') == null:
+		if event.is_action_pressed("ui_accept") and active:
+			get_tree().paused = true
+			var dialog = Dialogic.start(pickupTimeline)
+			dialog.pause_mode = Node.PAUSE_MODE_PROCESS
+			dialog.connect('timeline_end', self, 'unpause')
+			add_child(dialog)
+			
+			print("picked up")
+
+			Global.player_inventory.append(imagePath)
+			print(Global.player_inventory)
+
+			
+func unpause(timeline_name):
+	get_tree().paused = false
+	get_parent().remove_child(self)
+
 func _on_EventArea_body_entered(body):
 	if body.name == "Player":
 		active = true
