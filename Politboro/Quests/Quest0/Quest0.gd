594d085d93724cb4580fb8fc1739bde75f104513
roguejuan44 <elitethan@gmail.com>
2023-01-27 21:57:53 -0800

    Added Home
    Created apt bldg hallway, player apt, and decorated.

--- Politboro/Quests/Quest0/Quest0.gd
+++ Politboro/Quests/Quest0/Quest0.gd
@@ -6,5 +6,10 @@ func _init():
 	description = "Head to the Ministry of Energy office in the Party HQ and talk to your boss."
 	
 
-	
-EnergyMinister.introtext = 
+
+func _on_Quest0_ready():
+	if Global.activeQuest == "Q0":
+		#Global.player_initial_map_position = player_spawn_location
+		#Global.player_facing_direction = player_direction
+		if get_tree().change_scene("res://Map Scenes/CitySouth.tscn") != OK:
+			print("Error")
--- Politboro/World Objects/Player/Player.gd
+++ Politboro/World Objects/Player/Player.gd
@@ -50,4 +50,3 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
-
