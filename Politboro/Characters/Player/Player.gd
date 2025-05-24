fcc9027c10bdfc1d85d185fe24aaecbef38c70bb
ethantindall <elitethan@gmail.com>
2025-05-23 23:25:19 -0600

    actually fixed it now because Godot is dumb and the AnimationTree somehow disappeared.

--- Politboro/Characters/Player/Player.gd
+++ Politboro/Characters/Player/Player.gd
@@ -4,7 +4,8 @@ const ACCEL = 800
 const SPEED = 200
 const FRICTION = 1000
 
-#var velocity = Vector2.ZERO
+var is_facing_left = false
+var last_blend_position = Vector2.ZERO
 
 @onready var bodyNode = $Skeleton/Body
 @onready var shoesNode = $Skeleton/Shoes
@@ -15,15 +16,12 @@ const FRICTION = 1000
 @onready var hatNode = $Skeleton/Hat
 @onready var animation_player = $AnimationPlayer
 @onready var animationTree = $AnimationTree
-var last_direction = "D"
-var is_facing_left = false
+@onready var animationState = animationTree.get("parameters/playback")
 
 
-
+# -------- SYSTEM FUNCTIONS ---------
 func _ready():
-	#self.global_position = Global.player_initial_map_position
 	$"/root/Global".register_player(self)
-	
 	set_skeleton(bodyNode, Global.current_customization["body"])
 	set_skeleton(shoesNode, Global.current_customization["shoes"])
 	set_skeleton(bottomNode, Global.current_customization["bottom"])
@@ -31,41 +29,70 @@ func _ready():
 	set_skeleton(faceNode, Global.current_customization["face"])
 	set_skeleton(hairNode, Global.current_customization["hair"])
 	set_skeleton(hatNode, Global.current_customization["hat"])
+	Clock.time_changed.connect(_update_clock_display)
+
+#func _process(delta: float) -> void:
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized() 
+	
+	if input_vector.x < 0 and not is_facing_left:
+		is_facing_left = true
+		#for node in [bodyNode, shoesNode, bottomNode, topNode, faceNode, hairNode, hatNode]:
+		#	node.flip_h = true
+		$Skeleton.scale.x = -1
+	elif input_vector.x > 0 and is_facing_left:
+		is_facing_left = false
+		#for node in [bodyNode, shoesNode, bottomNode, topNode, faceNode, hairNode, hatNode]:
+		#	node.flip_h = false
+		$Skeleton.scale.x = 1
 
 
+	"""
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Move/blend_position", input_vector)
+		animationState.travel("Move")
+		velocity = velocity.move_toward(input_vector * SPEED, ACCEL * delta)
+		
+	else:   
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	"""
+	
+	if input_vector != Vector2.ZERO:
+		if input_vector != last_blend_position:
+			animationTree.set("parameters/Idle/blend_position", input_vector)
+			animationTree.set("parameters/Move/blend_position", input_vector)
+			last_blend_position = input_vector
+		if animationState.get_current_node() != "Move":
+			animationState.travel("Move")
+		velocity = velocity.move_toward(input_vector * SPEED, ACCEL * delta)
+	else:
+		if animationState.get_current_node() != "Idle":
+			animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
-func set_skeleton(custom_node, sprite_sheet):
-	var texture = load(sprite_sheet) as Texture2D
-	custom_node.texture = texture
-	custom_node.hframes = 5
-	custom_node.vframes = 4
-	custom_node.frame = 5
-
-func _process(delta):
-	#Clock.time_changed.connect(_update_clock_display)
-	get_input()
 	move_and_slide()
-
-
-
-func get_input():
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_direction * speed
-
-
-#func _physics_process(delta):
-#	if self.global_position != Global.player_position:
-#		Global.player_position = self.global_position
+	# YOU ONLY NEED TO UPDATE THIS SOMETIMES
+	#Global.player_position = self.global_position
+	
 	
 func _on_fade_in_player_animation_finished(anim_name: StringName) -> void:
 	print("wow")
 	$Fade_In_Canvas/Fade_In_ColorRect.modulate.a = 0.0  # Keep it transparent after animation ends
 
 func _update_clock_display(hour, minute):
-	var h = str(hour)
-	var m
-	if len(str(minute)) == 1:
-		m = "0" + str(minute)
-	else:
-		m = str(minute)
-	#d$HUD/ClockBG/ClockLabel.text = "%s:%s" % [h, m]
+	$HUD/ClockBG/ClockLabel.text = "%02d:%02d" % [hour, minute]
+
+
+# --------- HELPER FUNCTIONS ---------
+func set_skeleton(custom_node, sprite_sheet):
+	var texture = load(sprite_sheet) as Texture2D
+	custom_node.texture = texture
+	custom_node.hframes = 5
+	custom_node.vframes = 4
+	custom_node.frame = 5
